97dc1edf430ce707e90fc162ec3a93ced99a5e7c
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-03-12 12:02:04 +0000

    Update timer.js

--- Eventos tamplate/timer.js
+++ Eventos tamplate/timer.js
@@ -1,8 +1,11 @@
 client.on("ready", () => {
-    var content = "Ain";
+    var content = "Ain";//Pode-se aplicar um embed tambem
+ 
     var channel = client.guilds.cache
+    
         .get("IDdoSeuServer") // Id do Servidor
         .channels.cache.get("IDdoCanalDoServer"); //Id do canal onde a mensagem será enviada
+    
     setInterval(function() {
         channel.send(content);
     }, 1000 * 60 * 60 * 1);
