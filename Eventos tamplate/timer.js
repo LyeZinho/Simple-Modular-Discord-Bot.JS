8b43d3fb8936f914128f7a3730939c4130c5c4ef
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2021-03-12 11:57:41 +0000

    Update timer.js

--- Eventos tamplate/timer.js
+++ Eventos tamplate/timer.js
@@ -1,5 +1,5 @@
 client.on("ready", () => {
-    var content = "Inscreva-se no canal Hyouka";
+    var content = "Ain";
     var channel = client.guilds.cache
         .get("IDdoSeuServer") // Id do Servidor
         .channels.cache.get("IDdoCanalDoServer"); //Id do canal onde a mensagem será enviada
@@ -7,5 +7,4 @@ client.on("ready", () => {
         channel.send(content);
     }, 1000 * 60 * 60 * 1);
     channel.send(content);
-    console.log("É para se inscrever mesmo! Saco pela.");
 })
